bdeddc5e97a53660ecff8b19ed557217069dc83e
YH189 <yhrj4874@gmail.com>
2026-08-24 09:52:19 +0530

    Revert to plain text banner, ASCII art caused encoding issues

--- drico_bot/main.py
+++ drico_bot/main.py
@@ -1,20 +1,3 @@
-import os
-os.system('')
-
-BANNER = r""" 
-  ____       _             ____        _   
- |  _ \ _ __(_) ___ ___   | __ )  ___ | |_ 
- | | | | '__| |/ __/ _ \  |  _ \ / _ \| __|
- | |_| | |  | | (_| (_) | | |_) | (_) | |_ 
- |____/|_|  |_|\___\___/  |____/ \___/ \__|
-                                            """
-
-def print_banner():
-    cyan = '\033[96m'
-    reset = '\033[0m'
-    print(cyan + BANNER + reset)
-    print("Type 'help' for commands, or 'exit' to quit.\n")
-
 def safe_calculate(user_input):
     parts = user_input.split()
 
